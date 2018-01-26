08659ad43a7dea2db5a057842d9175e51a55511d
sparkplug77 <ram.thiagu@gmail.com>
2018-01-27 09:46:18 +1100

    Initial Commit

---
+++ Assets/Scripts/Morse.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morse : MonoBehaviour
+{
+    static Dictionary<string, char> morseCodeDictionary;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitializeMCDictionary();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void InitializeMCDictionary()
+    {
+        morseCodeDictionary = new Dictionary<string, char>()
+        {
+            {".-", 'a'},
+            {"-...", 'b'},
+            {"-.-.", 'c'},
+            {"-..", 'd'},
+            {".", 'e'},
+            {"..-.", 'f'},
+            {"--.", 'g'},
+            {"....", 'h'},
+            {"..", 'i'},
+            {".---", 'j'},
+            {"-.-" , 'k'},
+            {".-..", 'l'},
+            {"--" , 'm'},
+            {"-.", 'n'},
+            {"---", 'o'},
+            {".--.", 'p'},
+            {"--.-", 'q'},
+            {".-." , 'r'},
+            {"...", 's'},
+            {"-", 't'},
+            {"..-", 'u'},
+            {"...-", 'v'},
+            {".--", 'w'},
+            {"-..-", 'x'},
+            {"-.--", 'y'},
+            {"--..", 'z'},
+            {".----", '1'},
+            {"..---", '2'},
+            {"...--", '3'},
+            {"....-", '4'},
+            {".....", '5'},
+            {"-....", '6'},
+            {"--...", '7'},
+            {"---..", '8'},
+            {"----.", '9'},
+            {"-----", '0'},
+        };
+    }
+
+    void MorseCodeDecoder()
+    {
+
+    }
+}
